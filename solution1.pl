902a1b266611245103809df053a3d269dd5b126e
Jamie Quigley <jamie.e.quigley@gmail.com>
2020-02-26 15:20:09 +0000

    Task 1 Question 1

---
+++ solution1.pl
@@ -0,0 +1,23 @@
+/**/
+
+station(al, [metropolitan]).
+station(bg, [central]).
+station(br, [victoria]).
+station(bs, [metropolitan]).
+station(cl, [central]).
+station(ec, [bakerloo]).
+station(em, [bakerloo, northern]).
+station(eu, [northern]).
+station(fp, [victoria]).
+station(fr, [metropolitan]).
+station(ke, [northern]).
+station(kx, [victoria, metropolitan]).
+station(lg, [central]).
+station(ls, [central, metropolitan]).
+station(nh, [central]).
+station(oc, [bakerloo, central, victoria]).
+station(pa, [bakerloo]).
+station(tc, [central,northern]).
+station(vi, [victoria]).
+station(wa, [bakerloo]).
+station(ws, [victoria, northern]).
