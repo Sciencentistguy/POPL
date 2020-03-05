29616d9a954cf4d3984ddc5e5c30be4cad7866ba
Jamie Quigley <jamie.e.quigley@gmail.com>
2020-03-05 20:15:50 +0000

    Task 3 Question 4

--- solution3.rkt
+++ solution3.rkt
@@ -7,3 +7,15 @@
         [else (+ (car list) (sum (cdr list)))]
     )
 )
+
+(define (desc? list) 
+    (cond
+        [(null? (cdr (cdr list))) 
+            (> (car list) (car (cdr list)))
+        ]
+        [(> (car list) (car (cdr list)))
+            (desc? (cdr list))
+        ]
+        [else #f]
+    )
+)
