341b204c18fe09daf98c94c78b5b261ac29eb19c
Jamie Quigley <jamie.e.quigley@gmail.com>
2020-03-05 20:15:28 +0000

    Task 3 Question 3

--- solution3.pl
+++ solution3.pl
@@ -9,3 +9,10 @@
      NewRunningTotal is RunningTotal + Element,
      length(NewList, Len),
      ((Len is 0, Sum is NewRunningTotal); (Len>0, sum1(NewList, NewRunningTotal, Sum))).
+
+ desc(List) :-
+     List=[_];
+     List=[Head|NewList],
+     NewList=[NewHead|_],
+     Head>NewHead,
+     desc(NewList).
